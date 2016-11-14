311429d2e0fee537fc4c17115995938e4a38aeb5
kickscar <kickscar@gmail.com>
2016-11-14 15:46:27 +0900

    1st commit

---
+++ src/main/java/com/bit2016/aoptest/MyAspect.java
@@ -0,0 +1,54 @@
+package com.bit2016.aoptest;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+
+	@Before( "execution(ProductVo com.bit2016.aoptest.ProductService.find(..))" )
+	public void beforeAdvice() {
+		System.out.println( "call [before advice]" );
+	}
+	
+	@After( "execution( * *..*.ProductService.*(..) )" )
+	public void afterAdvice() {
+		System.out.println( "call [after advice]" );
+	}
+	
+	@AfterReturning( "execution( * *..*.ProductService.*(..) )" )
+	public void afterReturningAdvice() {
+		System.out.println( "call [afterReturning advice]" );
+	}
+	
+	@AfterThrowing( 
+		value="execution( * *..*.ProductService.*(..) )", 
+		throwing="ex" )
+	public void afterThrowingAdvice( Throwable ex ){
+		System.out.println( "call [afterThrowing advice :" + ex );
+	}
+	
+	@Around( "execution( * *..*.ProductService.*(..) )")
+	public Object aroundAdvice( ProceedingJoinPoint pjp ) throws Throwable {
+		//before
+		System.out.println( "call [around advice] : before" );
+		
+		//point cut 이 되는 메서드 호출 
+		Object[] parameters = { "camera" };
+		Object result = pjp.proceed( parameters );
+		
+		//after
+		System.out.println( "call [around advice] : after" );
+		
+		// 반환
+		return result;
+	}
+	
+}
